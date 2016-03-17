07a59b1733475183ca276f488c85d0887afd7c59
David <david@hashlabs.com>
2016-03-17 09:39:07 -0430

    Add bootstrap, add font-awesome, Add assets folder
    Add bootstrap with css and js, add assets folder to include images,
    scripts, styles, fonts, add font-awesome.

--- content/themes/hashcore/functions.php
+++ content/themes/hashcore/functions.php
@@ -116,9 +116,15 @@ add_action( 'widgets_init', 'hashcore_widgets_init' );
 function hashcore_scripts() {
 	wp_enqueue_style( 'hashcore-style', get_stylesheet_uri() );
 
+	wp_enqueue_style( 'hashcore-style-bootstrap', get_template_directory_uri() . '/assets/styles/bootstrap.min.css' );
+
+	wp_enqueue_style( 'hashcore-style-font-awesome', get_template_directory_uri() . '/assets/styles/font-awesome.min.css' );
+
 	wp_enqueue_script( 'hashcore-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
 
 	wp_enqueue_script( 'hashcore-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+
+	wp_enqueue_script( 'hashcore-js-bootstrap', get_template_directory_uri() . '/assets/scripts/bootstrap.min.js', array( 'jquery' ),'v3',true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
