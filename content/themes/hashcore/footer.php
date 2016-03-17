b9c9c69730870fd5f5e29ba95cc6f2bdb32f8626
David <david@hashlabs.com>
2016-03-17 14:10:57 -0430

    Add bootstrap class in theme

--- content/themes/hashcore/footer.php
+++ content/themes/hashcore/footer.php
@@ -12,7 +12,9 @@
 ?>
 
 	</div><!-- #content -->
-
+	</div><!-- #page -->
+	</div><!-- .container header-->
+<div class="container">
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hashcore' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'hashcore' ), 'WordPress' ); ?></a>
@@ -20,7 +22,7 @@
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'hashcore' ), 'hashcore', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
-</div><!-- #page -->
+</div> <!-- .container footer-->
 
 <?php wp_footer(); ?>
 
--- content/themes/hashcore/sidebar.php
+++ content/themes/hashcore/sidebar.php
@@ -11,7 +11,9 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
-<aside id="secondary" class="widget-area" role="complementary">
-	<?php dynamic_sidebar( 'sidebar-1' ); ?>
-</aside><!-- #secondary -->
+	<div class="col-md-3 col-xs-12" >
+		<aside id="secondary" class="widget-area" role="complementary">
+			<?php dynamic_sidebar( 'sidebar-1' ); ?>
+		</aside><!-- #secondary -->
+	</div><!-- .col-md-3>-->
+</div><!-- .row -->
