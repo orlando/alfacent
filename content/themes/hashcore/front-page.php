5bbabaeed14e156fbd6c9ab28f973af23706d3e0
David <david@hashlabs.com>
2016-04-18 16:47:18 -0430

    Fix layout in front page, and fix padding top
    Fix layout of front page and fix content is no show, and fix
    padding top of site-wrapper, and height of nav.

--- content/themes/hashcore/front-page.php
+++ content/themes/hashcore/front-page.php
@@ -13,28 +13,15 @@
  */
 
 get_header(); ?>
-<div class="container-fluid">
+<div class="container site-wrapper">
 	<div class="row">
 		<div class="col-xs-12">
 			<div id="primary" class="content-area">
 				<main id="main" class="site-main" role="main">
-
-					<?php
-					while ( have_posts() ) : the_post();
-
-						get_template_part( 'template-parts/content', 'page' );
-
-						// If comments are open or we have at least one comment, load up the comment template.
-						if ( comments_open() || get_comments_number() ) :
-							comments_template();
-						endif;
-
-					endwhile; // End of the loop.
-					?>
-
+					<?php the_content(); ?>
 				</main><!-- #main -->
 			</div><!-- #primary -->
-		</div><!--col-md-9 col-xs-12 -->
+		</div><!--col-xs-12 -->
 	</div><!-- .row -->
 </div><!-- container-->
 <?php
