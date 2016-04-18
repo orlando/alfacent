61d5bb49f689daae06bfbac0cf6d9177a8d6e834
David <david@hashlabs.com>
2016-04-18 16:56:59 -0430

    Change footer HTML structure

--- content/themes/hashcore/footer.php
+++ content/themes/hashcore/footer.php
@@ -11,15 +11,17 @@
 
 ?>
 
-<div class="container">
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info">
+<footer class="site-footer" role="contentinfo">
+	<div class="container">
+		<div class="site-footer-info">
 			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'hashcore' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'hashcore' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
 			<?php printf( esc_html__( 'Theme: %1$s by %2$s.', 'hashcore' ), 'hashcore', '<a href="http://automattic.com/" rel="designer">Automattic</a>' ); ?>
 		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
-</div> <!-- .container footer-->
+	</div> <!-- .container footer-->
+</footer><!-- #colophon -->
+<?php wp_footer(); ?>
+
 
 <?php wp_footer(); ?>
 
