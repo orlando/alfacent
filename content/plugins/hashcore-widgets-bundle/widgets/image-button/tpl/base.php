7f5c08a5c0f4d07e4ccdd273946e3d4c13973d23
David <david@hashlabs.com>
2016-05-18 12:03:25 -0400

    Fix indent on file php/html and less, remove tab

--- content/plugins/hashcore-widgets-bundle/widgets/image-button/tpl/base.php
+++ content/plugins/hashcore-widgets-bundle/widgets/image-button/tpl/base.php
@@ -15,22 +15,22 @@
 
 <?php
 $src = hashcore_widgets_get_attachment_image_src(
-	$image,
-	$size,
-	$image_fallback
+  $image,
+  $size,
+  $image_fallback
 );
 
 $attr = array();
 if ( ! empty( $src ) ) {
-	$attr = array(
-		'src' => $src[0],
-	);
+  $attr = array(
+    'src' => $src[0],
+  );
 
-	if ( ! empty( $src[1] ) ) $attr['width'] = $src[1];
-	if ( ! empty( $src[2] ) ) $attr['height'] = $src[2];
-	if ( function_exists( 'wp_get_attachment_image_srcset' ) ) {
-		$attr['srcset'] = wp_get_attachment_image_srcset( $image, $size );
-	}
+  if ( ! empty( $src[1] ) ) $attr['width'] = $src[1];
+  if ( ! empty( $src[2] ) ) $attr['height'] = $src[2];
+  if ( function_exists( 'wp_get_attachment_image_srcset' ) ) {
+    $attr['srcset'] = wp_get_attachment_image_srcset( $image, $size );
+  }
 }
 
 $classes = array( 'so-widget-image' );
@@ -41,18 +41,18 @@ if ( ! empty( $alt ) ) $attr['alt'] = $alt;
 ?>
 <div class="sow-image-container">
 <?php if ( ! empty( $url ) ) : ?><a href="<?php echo sow_esc_url( $url ) ?>" <?php if ( $new_window ) echo 'target="_blank"' ?>><?php endif; ?>
-	<div class="sow-image-wrapper">
-		<img <?php foreach($attr as $n => $v) echo $n.'="' . esc_attr($v) . '" ' ?> class="<?php echo esc_attr( implode(' ', $classes) ) ?>"/>
+  <div class="sow-image-wrapper">
+    <img <?php foreach($attr as $n => $v) echo $n.'="' . esc_attr($v) . '" ' ?> class="<?php echo esc_attr( implode(' ', $classes) ) ?>"/>
 
-		<?php if ( 'hidden' !== $title_position ) : ?>
-			<div class="sow-image-info">
-				<span class="sow-image-info-title"> <?php echo wp_kses_post( $title ) ?> </span>
-				<?php if ( ! empty( $description ) ) : ?>
-					<p class="sow-image-info-description"> <?php echo wp_kses_post( $description ) ?> </p>
-				<?php endif; ?>
-			</div>
-		<?php endif; ?>
+    <?php if ( 'hidden' !== $title_position ) : ?>
+      <div class="sow-image-info">
+        <span class="sow-image-info-title"> <?php echo wp_kses_post( $title ) ?> </span>
+        <?php if ( ! empty( $description ) ) : ?>
+          <p class="sow-image-info-description"> <?php echo wp_kses_post( $description ) ?> </p>
+        <?php endif; ?>
+      </div>
+    <?php endif; ?>
 
-	</div>
+  </div>
 <?php if ( ! empty( $url ) ) : ?></a><?php endif; ?>
 </div>
