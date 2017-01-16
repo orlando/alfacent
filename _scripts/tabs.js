d207afa64ac6cac6f117eb9fddffcdd8393fb2a2
Carlos <carlos@hashlabs.com>
2017-01-16 09:35:21 -0400

    initial commit
    alfacent front page

---
+++ _scripts/tabs.js
@@ -0,0 +1,25 @@
+var tabbableContent = {
+  init: function () {
+    showFirstTab();
+
+    $(".tabs li").click(function (e) {
+      if (!$(this).hasClass("active")) {
+        var tabNum = $(this).index();
+        var nthChild = tabNum + 1;
+
+        $(".tabs li.active").removeClass("active");
+        $(this).addClass("active");
+        $(".tab-containers li.active-tab").removeClass("active-tab").fadeOut({
+          duration: 200,
+          done: function () {
+            $(".tab-containers li:nth-child(" + nthChild + ")").addClass("active-tab").fadeIn(200);
+          }
+        });
+      }
+    });
+  }
+};
+
+function showFirstTab() {
+  $(".tab-containers li:nth-child(1)").show();
+}
--- content/plugins/hashcore-widgets-bundle/widgets/spacer/tpl/spacer-template.php
+++
@@ -1,6 +0,0 @@
-<?php
-$widget_title = wp_kses_post( $instance['widget_title'] );
-$spacer = wp_kses_post( $instance['spacer'] );
-?>
-
-<div class="spacer" style="height: <?php echo $spacer ?>px"></div>
